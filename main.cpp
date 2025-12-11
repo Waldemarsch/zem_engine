0b53549981c4e233d8932b8bccded517f227a802
V_Zemskov <vovazms@gmail.com>
2025-12-11 15:38:55 +0300

    Added vertex pipeline, added vertex shader

--- main.cpp
+++ main.cpp
@@ -8,6 +8,29 @@
 #include <GLFW/glfw3.h>
 
 
+GLuint VAO, VBO, shader;
+
+GLfloat triangleVertices[] = {
+    1.0f, 1.0f, 0.0f,
+    1.0f, -1.0f, 0.0f,
+    0.0f, 0.0f, 0.0f,
+};
+
+void DrawTriangle() {
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
 int main() {
     if (!glfwInit()) {
         std::printf("GLFW");
