c4274b38b92acf010fcb689ec1d785dc67665b11
V_Zemskov <vovazms@gmail.com>
2025-12-18 15:24:00 +0300

    Added renderer anchor, added recurse to CMakeLists.txt

---
+++ src/graphics/renderer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by val on 18/12/2025.
+//
+
+#include "renderer.h"
---
+++ src/graphics/renderer.h
@@ -0,0 +1,10 @@
+//
+// Created by val on 18/12/2025.
+//
+
+#ifndef OPENGLTRAIN_RENDERER_H
+#define OPENGLTRAIN_RENDERER_H
+
+class Renderer {};
+
+#endif  // OPENGLTRAIN_RENDERER_H
