44eb5c8dd1ab70e8ebc5435ef1e100cce8b990b7
Vela Juarez <vovazms@gmail.com>
2026-02-17 18:45:17 +0300

    Added physics_world realization

--- src/physics_world.cpp
+++ src/physics_world.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "zem/physics/physics_world.h"
+
+#include "internal/physics_data.h"
+
+namespace zem::physics {
+
+PhysicsWorld::PhysicsWorld() {
+  physics_data_ = std::make_unique<internal::PhysicsData>();
+}
+
+PhysicsWorld::~PhysicsWorld() = default;
+
+PhysicsWorld::PhysicsWorld(PhysicsWorld&&) noexcept = default;
+PhysicsWorld& PhysicsWorld::operator=(PhysicsWorld&&) noexcept = default;
+
+core::BodyHandle PhysicsWorld::CreateBody(const math::Vector3& position,
+                                          math::Real mass) {
+
+}
+
+}  // namespace zem::physics
